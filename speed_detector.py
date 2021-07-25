c1f2ec069363fd06b82561c27494b5c10df8cb90
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-07-26 02:30:37 +0530

    bug fixed

--- speed_detector.py
+++ speed_detector.py
@@ -16,130 +16,135 @@ WIDTH =1280
 HEIGHT = 720
 
 #estimate speed function
-def estimateSpeed(location1,location2):
-    d_pixels = math.sqrt(math.pow(location2[0]-location1[0],2)+math.pow(location2[1]-location1[1],2))
+def estimateSpeed(location1, location2):
+    d_pixels = math.sqrt(math.pow(location2[0] - location1[0], 2) + math.pow(location2[1] - location1[1], 2))
     ppm = 8.8
-    d_metres = d_pixels/ppm 
-    fps = 18 
-    speed = d_metres * fps * 3.6 
-    return speed 
+    d_meters = d_pixels / ppm
+    fps = 18
+    speed = d_meters * fps * 3.6
+    return speed
 
-#detect multiple objects
+#tracking multiple objects
 def trackMultipleObjects():
-    rectColor = (255,0,255)
+    rectangleColor = (0, 255, 0)
     frameCounter = 0
-    currentCarId = 0
-    fps = 0 
+    currentCarID = 0
+    fps = 0
 
     carTracker = {}
-    carNumber = {}
+    carNumbers = {}
     carLocation1 = {}
     carLocation2 = {}
+    speed = [None] * 1000
 
-    speed = [None]*1000 
-
-    out = cv2.VideoWriter('outTraffic.avi',cv2.VideoWriter_fourcc('M','J','P','G'),10,(WIDTH,HEIGHT))
+    out = cv2.VideoWriter('outTraffic.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 10, (WIDTH, HEIGHT))
 
     while True:
         start_time = time.time()
-        rc,frame = video.read()
+        rc, image = video.read()
+        if type(image) == type(None):
+            break
 
-        if type(frame) == type(None):
-            break 
+        image = cv2.resize(image, (WIDTH, HEIGHT))
+        resultImage = image.copy()
 
-        frame = cv2.resize(frame,(WIDTH,HEIGHT))
-        resultImage = frame.copy()
-        frameCounter += 1
-        carIDToDelete = []
+        frameCounter = frameCounter + 1
+        carIDtoDelete = []
 
         for carID in carTracker.keys():
-            trackingQuality = carTracker[carID].update(frame)
+            trackingQuality = carTracker[carID].update(image)
 
             if trackingQuality < 7:
-                carIDToDelete.append(carID)
+                carIDtoDelete.append(carID)
 
-        for carID in carIDToDelete:
-            print("Removing "+str(carID)+" from list of trackers.")
-            print("Removing "+str(carID)+" previous location.")
-            print("Removing "+str(carID)+" current location.")
+        
+        for carID in carIDtoDelete:
+            print("Removing carID " + str(carID) + ' from list of trackers. ')
+            print("Removing carID " + str(carID) + ' previous location. ')
+            print("Removing carID " + str(carID) + ' current location. ')
+            carTracker.pop(carID, None)
+            carLocation1.pop(carID, None)
+            carLocation2.pop(carID, None)
 
-            carTracker.pop(carID,None)
-            carLocation1.pop(carID,None)
-            carLocation2.pop(carID,None)
+        
+        if not (frameCounter % 10):
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            cars = carCascade.detectMultiScale(gray, 1.1, 13, 18, (24, 24))
 
-        if  not (frameCounter % 10):
-            grayScaleImage = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-            cars = carCascade.detectMultiScale(grayScaleImage,1.1,13,18,(24,24))
-
-            for (_x,_y,_w,_h) in cars :
+            for (_x, _y, _w, _h) in cars:
                 x = int(_x)
                 y = int(_y)
                 w = int(_w)
                 h = int(_h)
 
-                x_bar = x + 0.5*w
-                y_bar = y + 0.5*h 
+                x_bar = x + 0.5 * w
+                y_bar = y + 0.5 * h
 
-                matchCarID = None 
+                matchCarID = None
 
                 for carID in carTracker.keys():
-                    trackerPosition = carTracker[carID].get_position()
+                    trackedPosition = carTracker[carID].get_position()
 
-                    t_x = int(trackerPosition.left())
-                    t_y = int(trackerPosition.top())
-                    t_w = int(trackerPosition.width())
-                    t_h = int(trackerPosition.height())
+                    t_x = int(trackedPosition.left())
+                    t_y = int(trackedPosition.top())
+                    t_w = int(trackedPosition.width())
+                    t_h = int(trackedPosition.height())
 
-                    t_xbar = t_x + 0.5*t_w 
-                    t_ybar = t_y + 0.5*t_h 
+                    t_x_bar = t_x + 0.5 * t_w
+                    t_y_bar = t_y + 0.5 * t_h
 
-                    if((t_x <= x_bar <= (t_x + t_w)) and (t_y <= y_bar <= (t_y + t_h)) and (x <= t_xbar <= (x+w)) and (y <= t_ybar <= (y+ h))):
-                        matchCarID = carID 
+                    if ((t_x <= x_bar <= (t_x + t_w)) and (t_y <= y_bar <= (t_y + t_h)) and (x <= t_x_bar <= (x + w)) and (y <= t_y_bar <= (y + h))):
+                        matchCarID = carID
 
                 if matchCarID is None:
-                    print("Creating new tracker " + str(currentCarId))
+                    print(' Creating new tracker' + str(currentCarID))
 
                     tracker = dlib.correlation_tracker()
-                    tracker.start_track(frame,dlib.rectangle(x,y,x+w,y+h))
+                    tracker.start_track(image, dlib.rectangle(x, y, x + w, y + h))
 
-                    carTracker[currentCarId] = tracker 
-                    carLocation1[currentCarId] = x,y,w,h 
-                    currentCarId  += 1
-        
+                    carTracker[currentCarID] = tracker
+                    carLocation1[currentCarID] = [x, y, w, h]
+
+                    currentCarID = currentCarID + 1
+
         for carID in carTracker.keys():
             trackedPosition = carTracker[carID].get_position()
+
             t_x = int(trackedPosition.left())
             t_y = int(trackedPosition.top())
             t_w = int(trackedPosition.width())
             t_h = int(trackedPosition.height())
 
-            cv2.rectangle(resultImage,(t_x,t_y),(t_x + t_w,t_y + t_h),rectColor,4)
+            cv2.rectangle(resultImage, (t_x, t_y), (t_x + t_w, t_y + t_h), rectangleColor, 4)
 
-            carLocation2[carID] = [t_x,t_y,t_w,t_h]
+            carLocation2[carID] = [t_x, t_y, t_w, t_h]
+
         end_time = time.time()
 
-        if not(end_time == start_time):
+        if not (end_time == start_time):
             fps = 1.0/(end_time - start_time)
 
         for i in carLocation1.keys():
             if frameCounter % 1 == 0:
-                [x1,y1,w1,h1] = carLocation1[i]
-                [x2,y2,w2,h2] = carLocation2[i]
+                [x1, y1, w1, h1] = carLocation1[i]
+                [x2, y2, w2, h2] = carLocation2[i]
 
-                carLocation1[i] = [x2,y2,w2,h2]
+                carLocation1[i] = [x2, y2, w2, h2]
 
-                if [x1,y1,w1,h1] != [x2,y2,w2,h2]:
-                    if (speed[i] == None or speed[i] == 0) and y1 >=275 and y1 <= 285:
-                        speed[i] == estimateSpeed([x1,y1,w1,h1],[x2,y2,w2,h2])
+                if [x1, y1, w1, h1] != [x2, y2, w2, h2]:
+                    if (speed[i] == None or speed[i] == 0) and y1 >= 275 and y1 <= 285:
+                        speed[i] = estimateSpeed([x1, y1, w1, h1], [x1, y2, w2, h2])
 
-                    if speed[i] != None and y >= 180:
-                        cv2.putText(resultImage, str(int(speed[i]))+" km/h", int(x1 + w1/2), int(y1 - 5),cv2.FONT_HERSHEY_SIMPLEX,0.75,(0,0,100),2)
+                    if speed[i] != None and y1 >= 180:
+                        cv2.putText(resultImage, str(int(speed[i])) + "km/h", (int(x1 + w1/2), int(y1-5)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 100) ,2)
 
-        cv2.imshow("Result",resultImage)
+        cv2.imshow('result', resultImage)
+
         out.write(resultImage)
 
         if cv2.waitKey(1) == 27:
             break
+
     
     cv2.destroyAllWindows()
     out.release()
